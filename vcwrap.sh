fc0632df4c744ac7db0b1e386afea4d61be9f0d9
Amit Bakshi <ambakshi@gmail.com>
2015-04-18 15:20:25 -0700

    Check for vcinstall prereq

--- vcwrap.sh
+++ vcwrap.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+if ! test -d $HOME/.wine; then
+    echo >&2 "Please run vcinstall.sh first to setup your compiler and ~/.wine!!"
+    exit 2
+fi
+set -e
 TMP=$HOME/.wine/drive_c/windows/temp/$$.bat
 cat > "$TMP" <<EOF
 @set VC=%CommonProgramFiles%\\Microsoft\\Visual C++ for Python\\9.0\\
@@ -6,6 +11,7 @@ cat > "$TMP" <<EOF
 $(basename $0) %*
 EOF
 export WINEDEBUG=-all
+set +e
 wine cmd.exe /c "C:\\Windows\\Temp\\$(basename $TMP)" "$@"
 rc=$?
 rm -f "$TMP"
